89ae9bc3954959c72091e508a0f4aaa5633d860f
Eric Selin <eric.selin@gmail.com>
2021-01-20 09:13:33 +0100

    feat: Make working token authenticator

--- server/auth.test.ts
+++ server/auth.test.ts
@@ -1,15 +1,48 @@
 import { assertEquals } from "https://deno.land/std@0.81.0/testing/asserts.ts";
+import { FakeTime } from "https://deno.land/x/mock@v0.9.4/time.ts";
 import { getUserId } from "./auth.ts";
 
 const token =
   "eyJhbGciOiJSUzI1NiIsImtpZCI6Ijc4M2VjMDMxYzU5ZTExZjI1N2QwZWMxNTcxNGVmNjA3Y2U2YTJhNmYiLCJ0eXAiOiJKV1QifQ.eyJpc3MiOiJodHRwczovL2FjY291bnRzLmdvb2dsZS5jb20iLCJhenAiOiI0MDc0MDg3MTgxOTIuYXBwcy5nb29nbGV1c2VyY29udGVudC5jb20iLCJhdWQiOiI0MDc0MDg3MTgxOTIuYXBwcy5nb29nbGV1c2VyY29udGVudC5jb20iLCJzdWIiOiIxMTM1MjA3Nzg0MzU3NDMxNzIwNzgiLCJhdF9oYXNoIjoiYWpZT291TWNJVmZoSUF0MzRwMURzdyIsImlhdCI6MTYxMTA4OTM4OCwiZXhwIjoxNjExMDkyOTg4fQ.AejXjHc3vB9xHK0hVvGEpgJWftS8ksVaLqHo65LGSeODNyHD0uh42e9iDVsjCcNZEX64tSx1a_Ah0TDnEGorOKfJYXSYqmBE3RSwY2QHFhoP4lbgaEo6usyyeIIG5pcdCI5Vgir11zL4BfuuqoiTZK2t6TRlBCEqNeuA8-je3kjeNiISbPm5Hmx_qzPom2UNimPj4zLBv4h2O0sR98Dz9Hpra9RZwMPLZFWDb28CHoty3NFW1aeXHwCC0EpXVjbh7jB7OlzKLQhV_66QQeX9SBv_gx9h5vVnLWj341wL-utIS6-EQrmnAMN07dXyhgH1K8ErIwklsezDYrVhEqpywg";
 const sub = "113520778435743172078";
+const keys = [
+  {
+    "alg": "RS256",
+    "kty": "RSA",
+    "use": "sig",
+    "e": "AQAB",
+    "n":
+      "8Yb9hQAJroV6VKCsZZ6ylhVJqo0gsFa0Ca8ytzanKKWsCjo6RaqLjej7QKniTKwhUheCvbfLUqY9Mc6iMbA3gI-6_2lLQbbxExt6WUpf-CAEv1oUcnH_jA6X5Bdu4TdUX29s3D8J95d0eR8z8J1pe-7CjTBClx7lZd5xSRcoDXHDhzkwvc-EehYV46FsJyZCthLpAXvj81gpfycveavNFBMj-nlHKopZvhMcwbsK5JZ37wn2SxFigpfmIojheFVShJsNmLErHVC9HoHTC0iMibsKdyo7mk5QNM_rdBK-KjJhlQr8l7CktAqUJIQzkW8qC7tV7Hl0xicp6ylWZ-pj-Q",
+    "kid": "783ec031c59e11f257d0ec15714ef607ce6a2a6f",
+  },
+  {
+    "alg": "RS256",
+    "kid": "eea1b1f42807a8cc136a03a3c16d29db8296daf0",
+    "e": "AQAB",
+    "n":
+      "0zNdxOgV5VIpoeAfj8TMEGRBFg-gaZWz94ePR1yxTKzScHakH4F4wcMEyL0vNE-yW_u4pOl9E-hAalPa2tFv4fCVNMMkmKwcf0gm9wNFWXGakVQ8wER4iUg33MyUGOWj2RGX1zlZxCdFoZRtshLx8xcpL3F5Hlh6m8MqIAowWtusTf5TtYMXFlPaWLQgRXvoOlLZ-muzEuutsZRu-agdOptnUiAZ74e8BgaKN8KNEZ2SqP6vE4w16mgGHQjEPUKz9exxcsnbLru6hZdTDvXbX9IduabyvHy8vQRZsqlE9lTiOOOC9jwh27TXsD05HAXmNYiR6voekzEvfS88vnot2Q",
+    "use": "sig",
+    "kty": "RSA",
+  },
+];
+const clientId = "407408718192.apps.googleusercontent.com";
 
-Deno.test("returns user claim", async () => {
-  const request = new Request(
-    "http://localhost",
-    { headers: { cookie: `token=${token}` } },
-  );
-  const id = await getUserId(request);
-  assertEquals(id, sub);
+Deno.test("authenticator returns user claim", async () => {
+  const time = new FakeTime("2021-01-19 21:00");
+  try {
+    const request = new Request(
+      "http://localhost",
+      { headers: { cookie: `token=${token}` } },
+    );
+    const id = await getUserId(keys, clientId)(request);
+    assertEquals(id, sub);
+  } finally {
+    time.restore();
+  }
+});
+
+Deno.test("authenticator returns undefined if no token in request", async () => {
+  const request = new Request("http://localhost");
+  const id = await getUserId(keys, clientId)(request);
+  assertEquals(id, undefined);
 });
--- server/auth.ts
+++ server/auth.ts
@@ -1,41 +1,82 @@
 import { encode, RSA } from "https://deno.land/x/god_crypto@v1.4.8/mod.ts";
-import { decode } from "https://deno.land/x/djwt@v2.0/mod.ts";
+import { decode, Payload } from "https://deno.land/x/djwt@v2.0/mod.ts";
 import { getCookies } from "https://deno.land/std@0.83.0/http/cookie.ts";
+import type { JSONWebKey } from "https://deno.land/x/god_crypto@v1.4.8/src/rsa/common.ts";
 
-type UserIdGetter = (request: Request) => Promise<string | undefined>;
-
-const getGoogleAuthKey = async () => ({
-  "kid": "783ec031c59e11f257d0ec15714ef607ce6a2a6f",
-  "n":
-    "8Yb9hQAJroV6VKCsZZ6ylhVJqo0gsFa0Ca8ytzanKKWsCjo6RaqLjej7QKniTKwhUheCvbfLUqY9Mc6iMbA3gI-6_2lLQbbxExt6WUpf-CAEv1oUcnH_jA6X5Bdu4TdUX29s3D8J95d0eR8z8J1pe-7CjTBClx7lZd5xSRcoDXHDhzkwvc-EehYV46FsJyZCthLpAXvj81gpfycveavNFBMj-nlHKopZvhMcwbsK5JZ37wn2SxFigpfmIojheFVShJsNmLErHVC9HoHTC0iMibsKdyo7mk5QNM_rdBK-KjJhlQr8l7CktAqUJIQzkW8qC7tV7Hl0xicp6ylWZ-pj-Q",
-  "alg": "RS256",
-  "e": "AQAB",
-  "kty": "RSA",
-  "use": "sig",
-});
-
-export const getUserId: UserIdGetter = async (request) => {
+const getToken = (request: Request): string | undefined => {
   const cookies = getCookies(request);
-  const key = await getGoogleAuthKey();
-  const jwt = cookies.token;
-  try {
+  return cookies.token;
+};
+
+type TokenVerifier = (jwtToken: string) => Promise<Payload>;
+
+const verifySignatureAndDecode = (
+  jwks: JSONWebKey[],
+): TokenVerifier =>
+  async (jwtToken) => {
+    const { header, payload } = decode(jwtToken);
+    const key = jwks.find((jwk) => jwk.kid === header.kid);
+    if (!key) throw new Error(`JWK with id ${header.kid} not found`);
+
     // from https://stackoverflow.com/questions/62228271/how-to-verify-a-jwt-with-rs256-signature-in-deno
     const publicKey = RSA.parseKey(key);
     const rsa = new RSA(publicKey);
-    const [headerb64, payloadb64, signatureb64] = jwt.split(".");
-    // verify the signature based on the given public key
+    const [headerb64, payloadb64, signatureb64] = jwtToken.split(".");
     const verified = await rsa.verify(
       encode.base64url(signatureb64),
       headerb64 + "." + payloadb64,
       { algorithm: "rsassa-pkcs1-v1_5", hash: "sha256" },
     );
 
-    if (!verified) throw new Error("Token signature invalid");
+    if (verified) return payload;
 
-    const { payload } = decode(jwt);
-    return payload.sub;
-  } catch (error) {
-    console.log(error);
-    return undefined;
+    throw new Error("Token signature invalid");
+  };
+
+const verifyIssuer = (payload: Payload): Payload => {
+  if (
+    payload.iss === "https://accounts.google.com" ||
+    payload.iss === "accounts.google.com"
+  ) {
+    return payload;
   }
+  throw new Error(`Could not verify issuer (${payload.iss})`);
 };
+
+const verifyAudience = (clientId: string) =>
+  (payload: Payload): Payload => {
+    if (payload.aud === clientId) {
+      return payload
+    }
+    throw new Error(`Could not verify audience / client id (${payload.aud})`);
+  };
+
+const verifyExpiry = (payload: Payload): Payload => {
+  // exp claim is in seconds, Date.now() in milliseconds
+  if (payload.exp && payload.exp > Date.now() / 1000) {
+    return payload;
+  }
+  throw new Error(`Token has expired (${payload.exp})`);
+};
+
+const getUserIdFromPayload = ({ sub }: Payload): string => {
+  if (sub) return sub;
+  throw new Error("`sub` claim not found in payload");
+};
+
+type UserIdGetter = (request: Request) => Promise<string | undefined>;
+
+export const getUserId = (jwks: JSONWebKey[], clientId: string): UserIdGetter =>
+  async (request) => {
+    const token = getToken(request);
+
+    if (!token) return undefined;
+
+    return Promise.resolve(token)
+      // steps from https://developers.google.com/identity/protocols/oauth2/openid-connect#validatinganidtoken
+      .then(verifySignatureAndDecode(jwks))
+      .then(verifyIssuer)
+      .then(verifyAudience(clientId))
+      .then(verifyExpiry)
+      .then(getUserIdFromPayload);
+  };
